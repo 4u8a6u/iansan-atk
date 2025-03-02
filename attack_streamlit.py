b9a3d7d518a44eda41a4e818ffeb560138ec8314
4u8a6u <chelain486@gmail.com>
2025-03-02 17:34:07 +0800

    Update attack_streamlit.py

--- attack_streamlit.py
+++ attack_streamlit.py
@@ -1,8 +1,9 @@
 # attack_streamlit.py
 # 伊安珊夜魂值加攻模拟 - 基于Streamlit的交互式可视化工具
 # 感谢元宝deepseek帮助编写代码
+# “一切光明与美好的事物，都会随着不可逆转的熵增走向残破与毁灭。而宇宙的终点是皇女的未来国士，终结一切幻想的⌈幽夜净土⌋。”
 # 在线访问 https://iansan-atk.streamlit.app/
-# 本地部署使用指令 streamlit run attack_streamlit.py
+# 本地部署启动使用指令 streamlit run attack_streamlit.py
 # 默认访问本地8501端口 http://localhost:8501/
 
 import streamlit as st
